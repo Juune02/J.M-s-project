1ed3105336e7eb705e462c92b38dc36fbdd8864e
Juune02 <mykj6573@gmail.com>
2021-05-07 21:21:29 +0900

    Circular_graph(remake).py was updated by Juune02!

--- Circular_graph(remake).py
+++ Circular_graph(remake).py
@@ -9,7 +9,6 @@ from matplotlib import style
 font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
 rc('font', family=font_name)
 
-print('블랙서바이벌 영원회귀의 티어 분포표 \n')
 webpage = urllib.request.urlopen('https://dak.gg/bser/statistics/tier')
 soup = BeautifulSoup(webpage, 'html.parser')
 tables = soup.select('table')
@@ -30,6 +29,7 @@ for tier in list1:
 tier1 = [tier.strip("1") for tier in tier1]
 tier1.insert(0,"Immortal")
 tier1.insert(1,"Titan")
+tier1.reverse()
 TT1=0
 for s in range(22,26):
     TT1+=float(L2[s])
@@ -53,5 +53,5 @@ tier_size = [TT1,TT2,TT3,TT4,TT5,TT6,float(L2[1]),float(L2[0])]
 width_num = 0.4
 plt.axis('equal')
 plt.pie(tier_size, labels = tier1, autopct='%1.1f%%')
-plt.title('블랙서바이벌 영원회귀의 티어 분포표', fontsize=30)
+plt.title('영원회귀: 블랙서바이벌의 티어 분포 그래프(솔로 기준)', fontsize=30)
 plt.show()
